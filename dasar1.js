4fecd5b9934aac11f386a24eb37b4fdba235bb38
Haloio29 <aureliolucio5@gmail.com>
2023-08-01 20:41:44 +0700

    upload files

---
+++ dasar1.js
@@ -0,0 +1,38 @@
+// cara membuat variabel
+var a = "a"
+let b = "b"
+const c = "c"
+
+//variabel terdiri atas beberapa utuk menampung data
+// * string
+// * integer
+// * boolean
+// * array
+// * object
+
+// contoh
+// let umur = 20;
+// let benarkah = true;
+// let uang = 1000.00;
+let nama = 'rizky';
+let akhir = 'hanif';
+let panjang = nama + ' ' + akhir;
+
+console.log(panjang);
+
+// let 
+//       angka1 = 40;
+//       angka2 = 30;
+
+// console.log("Penjumlahan =",angka1+angka2);
+// console.log("Pengurangan =",angka1-angka2);
+// console.log("Perkaliana  =",angka1*angka2);
+// console.log("Pembagian   =",angka1/angka2);
+// console.log("Modulus     =",angka1%angka2);
+
+// alur pembacaan code
+// membaca code dari atas ke bawah & dari kiri ke kanan
+
+let saya = "aku"
+saya="kamu"
+console.log(saya);
---
+++ main.js
@@ -0,0 +1,14 @@
+//algoritma deskriptif
+
+/*
+cara memasukan baju ke dalam mesin cuci ;
+misal mesin cuci hanya muat 3 baju saja
+tetapi kita lagi butuh mencuci 4 baju
+
+langkah ;
+kita memasukan 3 baju sekaligus kedalam mesin cuci,
+jika kita memasukan 4 baju sekaligus maka mesin cuci akan memunculkan peringatan,
+ketika sudah memasukan baju kurang dari 3 atau 3 baju,
+baju kotor yang ada akan dikurangi dengan baju yang masuk ke dalam mesin cuci tersebut,
+lakukan hal tersebut jika baju yang ingin di cuci masih tersedia
+*/
