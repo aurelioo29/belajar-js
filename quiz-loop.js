eb828a40b696db96694946b857f7caae0b70b6c9
Haloio29 <aureliolucio5@gmail.com>
2023-08-05 08:02:54 +0700

    exersice looping

--- quiz-loop.js
+++ quiz-loop.js
@@ -10,8 +10,8 @@ let i = 0;
 console.log("* LOOPING PERTAMA");
 while(i < 10)
 {
- console.log(i);
- i++;
+      console.log(i);
+      i++;
 }
 
 let j = 10;
@@ -19,8 +19,8 @@ let j = 10;
 console.log("* LOOPING KEDUA");
 while(j >= 0)
 {
- console.log(j);
- j--;
+      console.log(j);
+      j--;
 }
 
 // 2. Melakukan Looping Menggunakan For
@@ -30,6 +30,17 @@ while(j >= 0)
 // menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
 //tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
 
+console.log("LOOPING PERTAMA");
+for(let i = 0; i < 10; i++)
+{
+      console.log(i);
+}
+console.log("LOOPING KEDUA");
+for(let i = 10; i >= 0; i--)
+{
+      console.log(i);
+}
+
 // 3. Angka Ganjil dan Genap
 // Hint: kamu akan menggunakan kondisional juga di kasus ini.
 // Problem
@@ -38,9 +49,22 @@ while(j >= 0)
 // Apabila angka counter adalah angka genap, tuliskan GENAP
 // Apabila angka counter adalah angka ganjil, tuliskan GANJIL
 
+for (let i = 0; i <= 100; i++)
+{
+      if(i % 2 == 0)
+      {
+            console.log(`${i} adalah angka GENAP`);
+      }
+      else
+      {
+            console.log(`${i} adalah angka GANJIL`);
+      }
+}
+
 //4. counter kelipatan
 // Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
 // Pada 3 perulangan baru ini periksa setiap angka counter:
 // Apabila bukan kelipatan yang ditentukan tidak perlu menuliskan apa-apa
 // Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5, dan kelipatan 10 dengan pertambahan 9, tuliskan:
 // "3 kelipatan 3"dan seterusnya.
+
