4d92fe998443d597b886d168d8f877f02ccea2a3
Haloio29 <aureliolucio5@gmail.com>
2023-09-23 11:28:51 +0700

    part 8.4 soal 1 & 2

--- week-2/part-8.4/soal-1.js
+++ week-2/part-8.4/soal-1.js
@@ -2,28 +2,15 @@
 Diberikan sebuah function palindrome(kata) yang menerima satu parameter. Function tersebut akan mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan. 
 Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'.
 */
-
-// function palindrome(kata) {
-//   let result = "";
-//   for (let i = kata.length - 1; i >= 0; i--) {
-//     // if (result !== kata) {
-//     //   console.log(`false`);
-//     // } else {
-//     //   console.log(`true`);
-//     // }
-//     result += kata[i];
-//   }
-//   return kata;
-// }
 function palindrome(kata) {
   let result = "";
   for (let i = kata.length - 1; i >= 0; i--) {
-    result += kata;
-    if (result === kata){
-        console.log("true")
-    } else {
-        console.log("false")
-    }
+    result += kata[i];
+  }
+  if (result !== kata) {
+    console.log("false");
+  } else {
+    console.log("true");
   }
   return result;
 }
@@ -34,4 +21,3 @@ palindrome("blanket"); // false
 palindrome("civic"); // true
 palindrome("kasur rusak"); // true
 palindrome("mister"); // fals
-
--- week-2/part-8.4/soal-3.js
+++ week-2/part-8.4/soal-3.js
@@ -1,10 +1,21 @@
 function hitungJumlahKata(kalimat) {
-    // you can only write your code here!
+  let kata = kalimat.split(" "); // Memecah teks menjadi kata-kata dengan spasi sebagai pemisah
+
+  // Menghitung panjang kata-kata, tetapi hanya menghitung kata yang tidak kosong
+  let jumlahKata = 0;
+  for (let i = 0; i < kata.length; i++) {
+    if (kata[i] !== "") {
+      jumlahKata++;
+    }
   }
-  
-  // TEST CASES
-  console.log(hitungJumlahKata('I have a dream')); // 4
-  console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
-  console.log(hitungJumlahKata('A song to sing')); // 4
-  console.log(hitungJumlahKata('I')); // 1
-  console.log(hitungJumlahKata('I believe I can code')); //  
+
+  return jumlahKata;
+}
+
+// TEST CASES
+console.log(hitungJumlahKata("I have a  dream ")); // 4
+console.log(hitungJumlahKata("Never eat shredded wheat or cake")); // 6
+console.log(hitungJumlahKata("A song to sing")); // 4
+console.log(hitungJumlahKata("I")); // 1
+console.log(hitungJumlahKata("I believe I can code")); //5
+console.log(hitungJumlahKata("")); // 1
