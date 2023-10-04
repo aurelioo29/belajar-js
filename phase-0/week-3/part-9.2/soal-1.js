f7c210813034c06c717474a62fdfb514abb24bb6
Haloio29 <aureliolucio5@gmail.com>
2023-10-04 15:49:50 +0700

    add quiz logic part 9.2

---
+++ phase-0/week-3/part-9.2/soal-1.js
@@ -0,0 +1,15 @@
+//cek apakah angka yang dikirim adalah angka prima atau bukan?
+//cek google bagi yang ga tau apa itu angka prima
+function angkaPrima(angka) {
+  // you can only write your code here!
+  if (angka > 1 && angka < angka) {
+    return console.log("Bilangan Tidak Prima");
+  }
+}
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
